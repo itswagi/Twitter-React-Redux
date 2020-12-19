9249284c336fa3af455be8aced676e7a56b08223
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-19 15:41:25 +0500

    Updated postsList to fetch from Twitter API

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React, {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import './postsList.css'
-import {selectAllPosts} from './postsSlice'
+import {selectAllPosts, fetchPosts} from './postsSlice'
 
 export const PostsList = () => {
+    const dispatch = useDispatch()
     const posts = useSelector(selectAllPosts)
 
+    const postStatus = useSelector(state => state.posts.status)
+
+    useEffect(() => {
+        if (postStatus === 'idle'){
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
+
     const renderedPosts = posts.map(post => (
-        <div className="post" key={post.id}>
+        <div className="post" key={post[0].id}>
             <div className="postHeader">
-                <h2>{post.name}</h2>
-                <h3>{post.id}</h3>
-                <h4>{post.timestamp}</h4>
+                <h2>{post[1].name}</h2>
+                <h3>{post[1].username}</h3>
+                <h4>{post[0].created_at}</h4>
             </div>
             <div className="postBody">
-                <span>{post.text}</span>
+                <span>{post[0].text}</span>
             </div>
         </div>
     ))
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,16 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axios from 'axios'
 
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/tweets/search/recent?query=pakistan&user.fields=name,username&expansions=author_id&tweet.fields=created_at', {
+        headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`
+        }
+    })
+    const data = response.data.data.map((post, i) => [post, response.data.includes.users[i]])
+    return data
+})
+
 const initialState = {
     posts: [],
     status: 'idle',
@@ -10,18 +20,24 @@ const initialState = {
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.status = 'succeeded'
+            state.posts = state.posts.concat(action.payload)
+        },
+        [fetchPosts.rejected]: (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        }
+    }
 })
 
 export const selectAllPosts = state => state.posts.posts
 
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-    const response = await axios.get('https://api.twitter.com/2/tweets/search/recent?query=pakistan&user.fields=name,username&expansions=author_id&tweet.fields=created_at', {
-        headers: {
-            'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`
-        }
-    })
-    return response
-})
+
 
 export default postsSlice.reducer
